d83f0d91b7798dc238fa53f81246b3dbdd3e53f5
Timoteo Eduardo de Carvalho Soutello <timoteo.soutello>
2020-11-19 15:45:12 -0300

    Adjust terraform workflow

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,6 +6,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
     storage_account_name = "satsoutello"
     container_name       = "terraform-state"
     key                  = "terraform-test"
